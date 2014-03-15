4b2f1e0cf7a12aef8568cb3d9101c1d6ef3c6425
Risto Keravuori <rkeravuori@fool.com>
2014-03-15 14:37:22 -0400

    Placeholder page for rendering

--- wedding/urls.py
+++ wedding/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+
+from www.views import index
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'wedding.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
